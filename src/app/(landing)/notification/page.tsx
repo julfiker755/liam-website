ae99e9039950d3523bb177087aec37ad0c42f375
almas hossain <almashossain7384@gmail.com>
2025-11-27 18:44:25 -0800

    notification section add

--- src/app/(landing)/notification/page.tsx
+++ src/app/(landing)/notification/page.tsx
@@ -13,6 +13,9 @@ import { X } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import IconBox from "@/components/reusable/Icon-box";
+import SubTitle from "@/components/reusable/title";
+import FavIcon from "@/favicon/favicon";
 
 const intState = {
   isDReq: false,
@@ -33,6 +36,61 @@ const ratingOptions: RatingOption[] = [
   { label: "Best", stars: 4 },
   { label: "Excellent", stars: 5 },
 ];
+
+type NotificationType = "default" | "tapToView" | "success" | "error";
+
+const notifications = [
+  {
+    title: "Order placed successfully",
+    date: "10th Nov, 2025 at 05:30 PM",
+    type: "default",
+    hasImage: true,
+    read: false,
+  },
+  {
+    title: "Order completed successfully",
+    date: "10th Nov, 2025 at 05:30 PM",
+    type: "success",
+    hasImage: true,
+    read: false,
+  },
+  {
+    title: 'Delivery request from "John Doe"',
+    date: "10th Nov, 2025 at 05:30 PM",
+    type: "tapToView",
+
+    showTapToView: true,
+    hasImage: true,
+    read: false,
+  },
+  {
+    title: "Your order has been accepted by the vendor",
+    date: "10th Nov, 2025 at 05:30 PM",
+    type: "success",
+    hasText: true,
+    read: false,
+  },
+  {
+    title: "Your order has been declined by the vendor",
+    date: "10th Nov, 2025 at 05:30 PM",
+    type: "error",
+    hasText: true,
+    read: true,
+  },
+];
+
+const getTextColor = (type: NotificationType) => {
+  switch (type) {
+    case "success":
+      return "text-[#2D9D1E]";
+    case "tapToView":
+      return "text-[#008BC0]";
+    case "error":
+      return "text-[#FF5445]";
+    default:
+      return "text-foreground";
+  }
+};
 
 const Notification = () => {
   const [deliveryReModal, setDeliveryReModal] = useGlobalState(intState);
@@ -62,13 +120,67 @@ const Notification = () => {
   const packagePrice = 250;
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Button
+    <div className="container px-4 mb-20">
+      <SubTitle svg={false} className="text-2xl my-10" text="Notification" />
+
+      <div className="space-y-3">
+        {notifications.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              if (item.showTapToView) {
+                setDeliveryReModal("isDReq", true);
+              }
+            }}
+            className={`flex flex-col sm:flex-row sm:items-center justify-between 
+                       bg-muted/50 py-4 px-4 rounded-lg gap-4 sm:gap-0  ${item.showTapToView ? "cursor-pointer" : ""}`}
+          >
+            {/* LEFT SIDE */}
+            <div className="flex items-start sm:items-center gap-4">
+              <IconBox>
+                <FavIcon name="noti" />
+              </IconBox>
+
+              <div className="flex flex-col">
+                <span
+                  className={`font-medium ${getTextColor(
+                    item.type as NotificationType
+                  )}`}
+                >
+                  {item.title}
+                </span>
+
+                <div className="flex flex-wrap items-center gap-2 text-sm text-muted-foreground">
+                  <span>{item.date}</span>
+
+                  {item.showTapToView && (
+                    <>
+                      <span>•</span>
+                      <span>Tap to view</span>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            {/* RIGHT SIDE ICON */}
+            <div className="text-foreground flex sm:block justify-end w-full sm:w-auto">
+              {item.read ? (
+                <FavIcon name="message_read" />
+              ) : (
+                <FavIcon name="message_unread" />
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* <Button
         onClick={() => setDeliveryReModal("isDReq", true)}
         className="text-xl font-semibold"
       >
         Notification
-      </Button>
+      </Button> */}
 
       {/* add delivery req */}
       <Modal2
